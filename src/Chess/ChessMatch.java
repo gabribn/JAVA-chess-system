0441b2e579294ab9c2d489ff2f3ae849565d3b34
Gabriel Reis <mnfgabriel@gmail.com>
2023-08-02 17:33:18 -0300

    Printing board, insertion of pieces, BoardExceptions.

---
+++ src/Chess/ChessMatch.java
@@ -0,0 +1,36 @@
+package Chess;
+
+import Chess.pieces.Rook;
+import boardgame.Board;
+
+public class ChessMatch {
+
+	private Board board;
+
+	public ChessMatch() {
+		board = new Board(8, 8);
+		initialSetup();
+	}
+
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColuns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColuns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
+	}
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPostion());
+	}
+
+	private void initialSetup() {
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+
+	}
+}
