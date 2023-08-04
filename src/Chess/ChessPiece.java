a0249a94da10a849a7693f1f66c0e0ad2517b6a2
Gabriel Reis <mnfgabriel@gmail.com>
2023-08-04 13:37:16 -0300

    Pieces movement count, add: Pawn, Bishop, Knight, Queen

--- src/Chess/ChessPiece.java
+++ src/Chess/ChessPiece.java
@@ -5,8 +5,9 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -16,13 +17,25 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public int getMoveCount(){
+		return moveCount;
+	}
 
 	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p!= null && p.getColor() != color;
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p.getColor() != color;
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
+	}
+
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
+
+	protected void decreaseMoveCount() {
+		moveCount--;
 	}
 }
